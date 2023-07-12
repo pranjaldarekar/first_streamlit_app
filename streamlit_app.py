130e27aec7499434522c61af25c3509b2a01d911
pranjal darekar <pranjaldarekar10@gmail.com>
2023-07-12 14:20:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ streamlit.stop()
   #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   #my_data_rows = get_fruit_load_list()
   #my_cnx.close()
-  2#streamlit.dataframe(my_data_rows)
+  #streamlit.dataframe(my_data_rows)
 
 #add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 #streamlit.write('Thanks for adding ', add_my_fruit)
